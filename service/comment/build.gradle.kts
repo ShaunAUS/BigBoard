fd8097ca8f17afdf71cb83956aad385e8988bb59
ShaunAUS <wnsqud70@gmail.com>
2025-04-10 11:47:33 +0900

    feat(Comment) : add comment dependencies

--- service/comment/build.gradle.kts
+++ service/comment/build.gradle.kts
@@ -1,3 +1,9 @@
 dependencies {
+    implementation(project(":storage:db"))
+    implementation(project(":common:snowflake"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    // 트랜잭션 사용을 위한 추가
+    implementation("org.springframework:spring-tx:${property("springTxVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 }
