aa0a8f33f054eb0f94398c8a7e6902b31a73bbfc
ShaunAUS <wnsqud70@gmail.com>
2025-04-10 11:45:12 +0900

    feat(Comment) : add comment entity

---
+++ storage/db/src/main/kotlin/big/board/entity/Comment.kt
@@ -0,0 +1,56 @@
+package big.board.entity
+
+import big.board.dto.comment.request.CommentCreateRequest
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
+@Entity
+class Comment(
+    content: String,
+    parentCommentId: Long,
+    articleId: Long,
+    writerId: Long,
+    isDeleted: Boolean,
+    createAt: LocalDateTime,
+
+    @Id
+    val commentId: Long? = null,
+) {
+    var content: String = content
+        protected set
+    var parentCommentId: Long = parentCommentId
+        protected set
+    var articleId: Long = articleId
+        protected set
+    var writerId: Long = writerId
+        protected set
+    var isDeleted: Boolean = isDeleted
+        protected set
+    var createAt: LocalDateTime = createAt
+        protected set
+
+    companion object {
+        fun of(commentId: Long, parentComment: Comment?, commentCreateRequest: CommentCreateRequest): Comment {
+            return Comment(
+                commentId = commentId,
+                content = commentCreateRequest.content,
+                parentCommentId = parentComment?.commentId ?: commentId,
+                articleId = commentCreateRequest.articleId,
+                writerId = commentCreateRequest.writerId,
+                isDeleted = false,
+                createAt = LocalDateTime.now(),
+            )
+        }
+    }
+
+    //상위 댓글이 없는경우 == 뎁스가 1인경우
+    fun isRoot(): Boolean {
+        return parentCommentId == commentId
+    }
+
+    fun delete() {
+        this.isDeleted = true
+    }
+
+}
